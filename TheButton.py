3c6c3a330c7e90d1545a247c843276e7bc09ef0f
Slend-K <kyleogthomas2008@outlook.com>
2024-02-06 19:17:23 +0000

    Testing committing directly from Visual Studio Code

---
+++ TheButton.py
@@ -0,0 +1,148 @@
+from tkinter import *
+import tkinter as Tk
+from PIL import Image, ImageTk
+import pygame
+import random
+
+def check_for_invalid_click_amounts():
+    with open("clicks.txt", "r+") as clicks_reader:
+        amount = int(clicks_reader.read().strip())
+        if int(amount) < 0 or int(amount) > 100:
+            print("Invalid number detected, fixing.")
+            clicks_reader.seek(0)
+            clicks_reader.write("0")
+            clicks_reader.truncate()
+            clicks_reader.close()
+
+def update_button():
+    with open("clicks.txt", "r") as clicks_reader:
+        amount_of_clicks.set(clicks_reader.read().strip())
+    window.after(75, update_button)
+
+pygame.mixer.init() # Initialising Pygame to play sound(s)
+
+background_music = pygame.mixer.Sound("background_music.mp3")
+pygame.mixer.music.load("background_music.mp3")
+pygame.mixer.Channel(3).play(background_music, loops=-1)
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(1)
+
+button_clickable = True
+
+def set_button_clickable(value):
+    global button_clickable
+    button_clickable = value
+
+def add_click():
+    global button_clickable
+    if button_clickable:
+        #pygame.mixer.music.load("button_click.mp3")
+        #pygame.mixer.music.play(loops=0)
+        pygame.mixer.Channel(0).play(pygame.mixer.Sound('button_click.mp3'))
+        with open("clicks.txt", "r+") as f:
+            amount_of_clicks = int(f.read().strip())
+            amount_of_clicks += 1
+            f.seek(0)
+            f.write(str(amount_of_clicks))
+            f.close()
+
+        set_button_clickable(False)
+        window.after(600, lambda: set_button_clickable(True))
+        reset_counter()
+
+def reset_counter():
+    with open("clicks.txt", "r") as f:
+        percentage_chance = int(f.read().strip())
+        f.close()
+
+    if reset_check(percentage_chance):
+        print("Unlucky! Resetting counter!")
+        with open("clicks.txt", "r+") as resetter:
+            amount_of_clicks = int(resetter.read().strip())
+            amount_of_clicks = 0
+            resetter.seek(0)
+            resetter.write(str(amount_of_clicks))
+            resetter.truncate()
+            resetter.close()
+            pygame.mixer.music.load("KABOOM.mp3")
+            pygame.mixer.Channel(2).play(pygame.mixer.Sound("KABOOM.mp3"))
+    else:
+        print("Lucky! Keeping counter as it should be.")
+
+def reset_check(chance):
+    # Ensuring the chance is between 0 and 100
+    chance = max(0, min(100, chance))
+
+    # Generating the random number to choose if the click counter should reset.
+    random_number = random.randint(0, 100)
+
+    # Check if it's less than or equal to the given percentage chance
+    return random_number <= chance
+
+alternating_text = [
+    "THE BUTTON clone, by Slender the Blender, inspired\nby Elendow's THE BUTTON game.",
+    "This is some text.",
+    "Press the button once more. You know you want to.",
+    "You should probably play the real version of this.\nMuch better than this version.",
+    "Life is button. Button is life.",
+    "There is no escape from the button.\nThe button knows where you are...",
+    "Don't be scared of the button, it can't hurt you.",
+    "Mum, can I get THE BUTTON?\nNo, we have THE BUTTON at home.\nTHE BUTTON at home:",
+    "Use an autoclicker, nobody will know!\nSeriously, I would never know how to make the game check.",
+    "Just ignore the ugly visual mishaps.\nPretend they don't exist.",
+    "FUN FACT: Cows have best friends!\nMaybe me and you could be besties!",
+    "Yes, the button above is indeed an image taken off Google.",
+    "Maybe I could code THE BUTTON into a Discord bot!\nNow that's an idea I definitely want to put action to one day.",
+    "FUN FACT: Bananas are berries but strawberries are not. Wow!",
+    "Is water wet?",
+    "Is a tomato a fruit?",
+    "If a tomato is a fruit, does that mean ketchup is a smoothie?",
+    "Coded by Slend, using a bit of Google...\n...and a small bit of ChatGPT.",
+    "This button... why was it ever made?",
+    "I'm surprised all this fit into only 146 lines!"
+]
+
+def update_changing_label_text():
+    new_text = random.choice(alternating_text)
+    changing_text_label.config(text=new_text)
+    window.after(7500, update_changing_label_text)
+
+bgcolour = "#f2e5d9"
+
+window = Tk.Tk()
+window.geometry("640x525")
+window.title("THE BUTTON Clone")
+window.config(bg=bgcolour)
+window.resizable(False, False)
+window.unbind_class("Button", "<Key-space>")
+icon = Image.open("button_main_image.png")
+icon_photo = ImageTk.PhotoImage(icon)
+window.wm_iconphoto(False, icon_photo)
+
+header = Tk.Label(window, text="THE BUTTON Clone", font=("Century Gothic", "32"), bg=bgcolour)
+header.pack(pady=2)
+
+amount_of_clicks = Tk.StringVar()
+
+with open("clicks.txt", "r") as clicks_reader:
+    amount_of_clicks.set(int(clicks_reader.read()))
+
+image_filepath = "C:\\Users\\kyleo\\OneDrive\\Documents\\Desktop\\VS Code\\!Things I Coded or Am Coding\\Python\\Games\\THE BUTTON Clone\\button_main_image.png"
+image = Image.open(image_filepath)
+main_button_photo = ImageTk.PhotoImage(image)
+
+# mainbutton = Tk.Button(window, textvariable=amount_of_clicks, image=PhotoImage("C:\\Users\\kyleo\\OneDrive\\Documents\\Desktop\\VS Code\\!Things I Coded or Am Coding\\Python\\Games\\THE BUTTON Clone\\button_main_image.png"), command=add_click) # change filepath to that which is stored on the usb when you can
+mainbutton = Tk.Button(window, textvariable=amount_of_clicks, image=main_button_photo, command=add_click, bg=bgcolour, state="normal") # change filepath to that which is stored on the usb when you can
+mainbutton.place(x="320", y="290", anchor="center")
+
+amt_of_clicks_label = Tk.Label(window, textvariable=amount_of_clicks, font=("Century Gothic", "40"), bg=bgcolour)
+amt_of_clicks_label.place(x="320", y="90", anchor="center")
+
+changing_text_label = Tk.Label(window, text="THIS IS A TEST", font=("Century Gothic", "16"), bg=bgcolour)
+changing_text_label.place(x="320", y="480", anchor="center")
+
+update_button()
+check_for_invalid_click_amounts()
+window.after(0, update_changing_label_text)
+
+window.mainloop()
---
+++ readme.md
@@ -0,0 +1,41 @@
+# THE BUTTON by Slend
+A little coding project embarked on by Slender the Blender, over about four or five days. I'll admit, nothing much about this is original apart from the code and the text at the bottom (some were inspired by the real game). Why? This is just a clone of a game someone else made. In short I had a project idea and I put it to action.
+
+Coded mostly by Slend, using Google and a little bit of ChatGPT as help for the bits I couldn't quite do myself.
+
+The amount of clicks is stored in *clicks.txt* because I wanted it to, like the real THE BUTTON game, track the amount of clicks even after closing and re-opening the game. Yes I know there definitely is a better way of doing it but hey-ho.
+
+Based off of [THE BUTTON by Elendow](https://store.steampowered.com/app/1999740/THE_BUTTON_by_Elendow/).
+
+Music was free to use from Pixabay, but I think it's only right if I [give credit where credit's due](https://pixabay.com/music/beats-waiting-room-122773/).
+
+Image for the button itself was taken randomly off Google. Luckily, it was taken from somewhere where it is [royalty-free, from Pixabay](https://pixabay.com/illustrations/red-button-circle-round-choose-1217969/)
+
+*button_click.mp3* was taken from [MyInstants](https://www.myinstants.com/en/instant/button-click-93467/). 
+
+*KABOOM.mp3* was also taken from [MyInstants](https://www.myinstants.com/en/instant/explosion/)
+
+It don't look pretty, but I'm happy with it.
+
+## LIBRARIES USED
+- Tkinter
+  - Window, buttons, text, etc..
+- Pygame
+  - Sounds
+- PIL
+  - Making images compatible with Tkinter
+- Random
+  - Choosing if the counter should reset or not.
+
+## TO-DO
+
+- Track total click count
+- Track best click count (most clicks before resetting)
+- Track reset count
+- Add a settings menu
+  - Add a dark mode and a button to toggle it.
+
+## USAGE
+Feel free to download this, edit it, do whatever.
+
+If you use this to make any sort of tutorial or use it on any websites, whatever, please credit me and Elendow, more importantly Elendow, though, as this is just a clone of his game as explained above.
